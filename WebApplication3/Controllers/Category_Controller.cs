f2b24f90588b54d885b947f2c1895f9afb69762a
Vuqarmamedov <vuqar.mamedovv@gmail.com>
2017-08-13 19:12:45 +0400

    Add project files.

---
+++ WebApplication3/Controllers/Category_Controller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class Category_Controller : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+        public ActionResult Details()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication3/Controllers/MyNewController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class MyNewController : Controller
+    {
+        // GET: MyNew
+        [HttpGet]
+        public ActionResult First()
+            
+        {
+            Product mehsul = new Product();
+            mehsul.id = 1;
+            mehsul.name = "Alma";
+            mehsul.desc = "yasil alma";
+            return View(mehsul);
+        }
+        [HttpPost]
+        public ActionResult Second()
+        
+        {
+            return View();
+        }
+    }
+}
